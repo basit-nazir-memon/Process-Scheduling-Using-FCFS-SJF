9017ef0ab8bc8a46e92ebfd05447b5e15b691bbf
Basit Nazir <basitnazir585@gmail.com>
2023-06-13 23:21:00 +0500

    Update main.cpp

--- Project7/main.cpp
+++ Project7/main.cpp
@@ -1,21 +1,9 @@
-#include "Maze.h"
 #include "FeedbackQueue.h"
 int main() {
-    cout << "-------------------- Question : 01 -----------------------\n";
-
+    cout << "-------------------- Process Scheduling Using FCFS AND SJF -----------------------\n";
     Driver driver;
     cout << "The Processess in Waiting Queue Are: \n";
     driver.getWaitingQueue().print();
     driver.execute();
-
-    /*cout << "Multi Level Queue Name: " << d.getName() << endl;
-    cout << "\n\n\n\n\nAfter Execution, The Processess in Waiting Queue Are: \n";
-    driver.getWaitingQueue().print();*/
-
-    cout << "\n\n\n\n\n\n\n\n\n\n\n\n-------------------- Question : 02 -----------------------\n";
-
-   
-	MazeSolver maze;
-	maze.solve();
-	return 0;
+    return 0;
 }
